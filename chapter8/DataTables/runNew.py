0559a01a8ec264c82f985cee01137c0ab7e251d4
nitigan.n <nitigan.n@fitm.kmutnb.ac.th>
2020-09-23 18:18:15 +0700

    create DB

--- chapter8/DataTables/runNew.py
+++ chapter8/DataTables/runNew.py
@@ -1,40 +1,39 @@
 from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from datatables import ColumnDT, DataTables
+from datatables import column_dt, datatables
 import datetime
 import random
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///sites.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sites.db'
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    __tablename__='user'
+class User(db.Model) :
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40))
     phone = db.Column(db.String(10))
     birthday = db.Column(db.Date)
 
-@app.route('/', methods=['GET','POST'])
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def index() :
     return render_template('index.html')
 
-@app.route('/load_table_sql',methods =['GET','POST'])
-def load_table_sql():
+@app.route('/load_table_sql', methods=['GET', 'POST'])
+def load_table_sql() :
     columns = [
-        ColumnDT(User.id),
-        ColumnDT(User.name),
-        ColumnDT(User.phone),
-        ColumnDT(User.birthday),
+        column_dt(User.id),
+        column_dt(User.name),
+        column_dt(User.phone),
+        column_dt(User.birthday),
     ]
     query = db.session.query().select_from(User)
-    rowTable = DataTables(request.args.to_dict(),query,columns)
-
+    rowTable = datatables.DataTables(request.args.to_dict(), query, columns)
     # returns what is needed by DataTable
     return rowTable.output_result()
-
-def random_date():
-    start_date = datetime.date(1997,1 ,1)
+    
+def random_date() :
+    start_date = datetime.date(1997, 1, 1)
     end_date = datetime.date(2010, 1, 1)
 
     time_between_dates = end_date - start_date
@@ -44,17 +43,15 @@ def random_date():
 
     return random_date
 
-def create_db_user():
-    db.drop_all()
+def create_db_user() :
     db.create_all()
-    for i in range(100):
-        user = User(name='user'+str(i),phone='phone'+
-                    str(i),birthday=random_date())
+    for i in range(10) :
+        user = User(name='user'+str(i), phone='phone' + 
+                    str(i), birthday=random_date())
         db.session.add(user)
     db.session.commit()
     print('create done')
-    #show_db_user()
+    show_db_user()
 
-create_db_user()
-if __name__ == "__main__":
+if __name__ == "__main__" :
     app.run( debug=True)
